29f6a177c5b49835ec84a64ad6563cf23860f715
Muzafar <taranqi1214@gmail.com>
2022-03-07 22:59:24 -0500

    JS practice

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
-let count = 0
+let count = 10
 console.log(count)
 
-let myAge = 32
-console.log(myAge)
 
-let humanDogRatio = 7
 
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
+// let myAge = 32
+// console.log(myAge)
+
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
