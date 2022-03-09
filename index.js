2c21cc16b76dbe1df52ac1e61f4440e1c0c49d18
Muzafar <taranqi1214@gmail.com>
2022-03-08 20:53:44 -0500

    added hello msg

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-document.getElementById("count-el").innerText = 1000
+document.getElementById("count-el").innerText = 100
 let count = 10
 console.log(count)
 
@@ -8,7 +8,10 @@ console.log(count)
 let myAge = 32
 console.log(myAge)
 
-let humanDogRatio = 7
+let humanDogRatio = 10
 
 let myDogAge = myAge * humanDogRatio
 console.log(myDogAge)
+
+let msg = "Hello Class"
+console.log(msg)
