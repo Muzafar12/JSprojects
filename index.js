665d190c77a801fcf184e70ce51d41b97b16d683
Muzafar <taranqi1214@gmail.com>
2022-03-08 20:25:32 -0500

    new changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
+document.getElementById("count-el").innerText = 1000
 let count = 10
 console.log(count)
 
 
 
-// let myAge = 32
-// console.log(myAge)
+let myAge = 32
+console.log(myAge)
 
-// let humanDogRatio = 7
+let humanDogRatio = 7
 
-// let myDogAge = myAge * humanDogRatio
-// console.log(myDogAge)
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
